f8a8f0e2a1ee9376b306f592c48b9013414134d8
aclarke9691 <aclarke9691@bths.edu>
2018-10-11 14:36:22 -0400

    hasdvasdvas

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,8 +1,14 @@
-public class Scanner {
-    public static void main(String name)
+import java.util.Scanner;
+
+public class guessANumber {
+    public static void main(String [] args)
 
     {
-        Scanner input = newScanner(System.in);
+        Scanner input = new Scanner(System.in);
+        int x= (int) Math.random() *11 + 100;
+        System.out.println("I am thinking of a number between 1-100. Guess it hehehehe.");
+        String number = input.nextLine;
+
 
     }
 }
