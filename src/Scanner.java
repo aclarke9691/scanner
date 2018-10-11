e09803b1acc0c615758e79c1e532caeaf4cff758
aclarke9691 <aclarke9691@bths.edu>
2018-10-11 14:44:56 -0400

    okayyy

--- src/Scanner.java
+++ src/Scanner.java
@@ -7,7 +7,19 @@ public class guessANumber {
         Scanner input = new Scanner(System.in);
         int x= (int) Math.random() *11 + 100;
         System.out.println("I am thinking of a number between 1-100. Guess it hehehehe.");
-        String number = input.nextLine;
+        int number = input.nextInt();
+        while (x!= number)
+        {
+            if (number>x)
+            {
+                System.out.println("Guess lower hehe");
+            }
+            else
+            {
+                System.out.println("Guess higher hehe");
+            }
+
+        }
 
 
     }
