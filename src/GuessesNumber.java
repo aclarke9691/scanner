3bf435ec24b588e70cbfe0cf7caf6d4c24b86a87
aclarke9691 <aclarke9691bths.edu>
2018-10-12 14:47:17 -0400

    Updatee

---
+++ src/GuessesNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GuessesNumber {
+    public static void main(String [] args)
+    {
+        Scanner input= new Scanner(System.in);
+        int x = (int) (Math.random() * 11);
+        System.out.println("I am going to guess the number you are thinking of. My first guess is" + x);
+        String response = input.nextLine();
+        while ( response != "correct")
+        {
+
+        }
+        if (response == "correct")
+        {
+            System.out.println("Yay I got it right! HEE HEE!");
+        }
+    }
+}
