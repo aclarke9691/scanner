49e7b3e2b1669e1ffd1075edfa8c2854510c5b0f
aclarke9691 <aclarke9691@bths.edu>
2018-10-14 19:50:51 -0400

    nvm

--- src/GuessesNumber.java
+++ src/GuessesNumber.java
@@ -5,22 +5,41 @@ public class GuessesNumber {
     {
         String response = "";
         Scanner input= new Scanner(System.in);
-        int x = (int) (Math.random() * 101);
+        //int x = (int) (Math.random() * 101);
+        int x= 50;
+        int y=25;
         System.out.println("I am going to guess the number you are thinking of. My first guess is " + x);
         response = input.nextLine();
         while ( response.equals("higher") || response.equals("lower"))
         {
             if (response.equals("higher"))
             {
-                x= x+1;
-                System.out.println("Is it " + x);
+                x= x+y;
+                System.out.println("Is it " + x + "?");
                 response= input.nextLine();
+                y=15;
+                x=x+y;
+                System.out.println("Is it " + x + "?");
+                response= input.nextLine();
+                y=10;
+                x=x+y;
+                System.out.println("Is it " + x + "?");
+                response= input.nextLine();
+                y=5;
+                x=x+y;
+                while(x<=100 && response.equals("higher")) {
+                    System.out.println("Is it " + x + "?");
+                    response= input.nextLine();
+                    y = 1;
+                    x = x + y;
+                }
             }
             if (response.equals("lower"))
             {
-                x= x-1;
-                System.out.println("Is it " + x);
+                x= x-y;
+                System.out.println("Is it " + x + "?");
                 response= input.nextLine();
+                y=y-1;
             }
 
         }
